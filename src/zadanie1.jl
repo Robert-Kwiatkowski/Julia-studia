31be31658b210eb7729fd4162f7599637a5cd993
Robert-Kwiatkowski <s169655@student.pg.edu.pl>
2020-04-15 16:11:00 +0200

    Pierwsze zadanie

---
+++ src/zadanie1.jl
@@ -0,0 +1,8 @@
+module zadanie1
+
+"Funkcja przyjmuje jeden argument (liczbe) i zwraca wyrazenie: x^3 + x^2 + x + 5"
+funkcja1(x) = x^3 + x^2 + x + 5
+
+export funkcja1
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,4 @@
+using zadanie1, Test
+
+@test funkcja1(10) == 1115
+@test funkcja1(3) == 44
